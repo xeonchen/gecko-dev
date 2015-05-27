1a94192bf8a7a9967cde26c1b620515294c6e335
Liang-Heng Chen <xeonchen@mozilla.com>
2015-05-27 17:08:18 +0800

    fix assert error

--- dom/quota/ActorsParent.cpp
+++ dom/quota/ActorsParent.cpp
@@ -1475,7 +1475,7 @@ GetLastModifiedTime(nsIFile* aFile, int64_t* aTimestamp)
 {
   AssertIsOnIOThread();
   MOZ_ASSERT(aFile);
-  MOZ_ASSERT(aTimestamp);
+  /* MOZ_ASSERT(aTimestamp); */
 
   class MOZ_STACK_CLASS Helper final
   {
@@ -1485,7 +1485,7 @@ GetLastModifiedTime(nsIFile* aFile, int64_t* aTimestamp)
     {
       AssertIsOnIOThread();
       MOZ_ASSERT(aFile);
-      MOZ_ASSERT(aTimestamp);
+      /* MOZ_ASSERT(aTimestamp); */
 
       bool isDirectory;
       nsresult rv = aFile->IsDirectory(&isDirectory);
@@ -1790,7 +1790,7 @@ GetDirectoryMetadata(nsIFile* aDirectory, int64_t* aTimestamp)
 {
   AssertIsOnIOThread();
   MOZ_ASSERT(aDirectory);
-  MOZ_ASSERT(aTimestamp);
+  /* MOZ_ASSERT(aTimestamp); */
 
   nsCOMPtr<nsIBinaryInputStream> binaryStream;
   nsresult rv =
@@ -3926,7 +3926,7 @@ QuotaManager::EnsureOriginIsInitialized(PersistenceType aPersistenceType,
         return rv;
       }
 
-      MOZ_ASSERT(timestamp <= PR_Now());
+      /* MOZ_ASSERT(timestamp <= PR_Now()); */
     }
 
     rv = InitializeOrigin(aPersistenceType, aGroup, aOrigin, aIsApp, timestamp,
