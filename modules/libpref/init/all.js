534b94fe02423ca2315e50a66e3d8355e5d3c32c
Liang-Heng Chen <xeonchen@mozilla.com>
2015-07-15 14:28:57 +0800

    Bug 1180596 - Part 2: make customizable settings of Presentation API; r=schien

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4870,6 +4870,8 @@ pref("dom.beforeAfterKeyboardEvent.enabled", false);
 // Presentation API
 pref("dom.presentation.enabled", false);
 pref("dom.presentation.tcp_server.debug", false);
+pref("dom.presentation.discovery.enabled", true);
+pref("dom.presentation.discoverable", false);
 
 #ifdef XP_MACOSX
 // Use raw ICU instead of CoreServices API in Unicode collation
